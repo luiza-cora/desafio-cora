e81a6d7c41945fc3124840c38dd6031fdc88a69b
Luiza Moruz <luizamoruz@cora.com.br>
2022-06-22 11:07:31 -0300

    Configuracoes iniciais do projeto

---
+++ desafio-cora/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  desafio-cora
+//
+//  Created by Cora on 22/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
