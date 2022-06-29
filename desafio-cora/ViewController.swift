4aa313ead06e20d57233fe6699a810d9ec9e3f4b
Luiza Moruz <luizamoruz@cora.com.br>
2022-06-29 11:48:25 -0300

    Cria IntroView

--- desafio-cora/ViewController.swift
+++ desafio-cora/ViewController.swift
@@ -9,11 +9,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .red
-    
+    override func loadView() {
+        self.view = IntroView()
+    }
 }
 
-}
 
